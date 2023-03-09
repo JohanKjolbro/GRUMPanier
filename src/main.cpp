ac7f669e9644025efccaf4f08ceff01c89b9a5b9
johan <johan.kjolbro@gmail.com>
2023-03-08 20:10:05 -0400

    LCD+Connexion reseau et serveur

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <Wifi.h>
+#include <LiquidCrystal.h>
 
 
 //Declaration de variables relatifs au wifi
@@ -8,8 +9,24 @@ const char* password = "lespaiens27";
 
 WiFiClient client;
 
-const char* host = "10.0.0.84";
+const char* host = "10.0.0.96";
 const int port = 1500;
+
+//Declaration de variables relatifs au lcd
+const int rs=13, en=12, d4=14, d5=27 , d6=26, d7=25;
+LiquidCrystal lcd(rs,en, d4, d5, d6, d7);
+
+int t;
+
+class afficheur
+{
+
+};
+
+class connexion
+{
+
+};
 
 
 
@@ -17,10 +34,17 @@ void setup() {
 
   //moniteur
   Serial.begin(115200);
-  Serial.println("testing");
+  Serial.println("testing23");
+  //lcd
+  lcd.begin(16,2);
+  
+  
+  
 
+  
   //Connexion au reseau
   Serial.println("\nConnexion au réseau");
+  lcd.print("Connexion WIFI");
   WiFi.begin(ssid,password);
   while(WiFi.status() != WL_CONNECTED)
   {
@@ -28,25 +52,32 @@ void setup() {
     delay(100);
   }
   Serial.print("Connecté au réseau");
-
+  lcd.print("Connecté WIFI");
 
 
   //Connexion au serveur
   Serial.println("\nConnexion au serveur");
+  lcd.println("\nConnexion server");
   while(!client.connect(host,port))
   {
     Serial.print(".");
     delay(100);
   }
   Serial.print("Connecté au serveur");
-  client.print("Hello testing");
-
-  //hello
+  Serial.print("Connecté server");
+  client.print("test1\n");
 
 }
 
+
 void loop() {
+
+  client.println(t++);
   
-  client.print("hello \n");
+  lcd.setCursor(0,0);
+  lcd.print("hello loop");
+  lcd.setCursor(0,1);
+  lcd.print("hello loop 2");
+
 
 }
